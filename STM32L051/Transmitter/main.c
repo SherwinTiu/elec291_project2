26fd6bae33c10200fd9426a3e7e90e36c3edadde
ashkechupp <ashkechup14@gmail.com>
2023-03-22 14:56:48 -0700

    update

--- STM32L051/Transmitter/main.c
+++ STM32L051/Transmitter/main.c
@@ -4,7 +4,7 @@
 #include "../Common/Include/serial.h"
 
 #define F_CPU 32000000L
-#define DEF_F 16011L // 16 kHz freq
+#define DEF_F 16200L // 16.20 kHz freq (frequency with highest output)
 
 volatile int PWM_Counter = 0;
 volatile unsigned char pwm1=100, pwm2=100;
@@ -46,11 +46,11 @@ void TIM2_Handler(void)
 //      PC15 -|3       30|- PB7
 //      NRST -|4       29|- PB6
 //      VDDA -|5       28|- PB5
-//       PA0 -|6       27|- PB4
-//       PA1 -|7       26|- PB3
-//       PA2 -|8       25|- PA15
-//       PA3 -|9       24|- PA14
-//       PA4 -|10      23|- PA13
+//       PA0 -|6       27|- PB4 (reverse button)
+//       PA1 -|7       26|- PB3 (forward button)
+//       PA2 -|8       25|- PA15 (right button)
+//       PA3 -|9       24|- PA14 (left button)
+//       PA4 -|10      23|- PA13 (switch mode button)
 //       PA5 -|11      22|- PA12 (h bridge pin 2)
 //       PA6 -|12      21|- PA11 (h bridge pin 1)
 //       PA7 -|13      20|- PA10 (Reserved for RXD)
@@ -59,7 +59,10 @@ void TIM2_Handler(void)
 //       VSS -|16      17|- VDD
 //             ----------
 
-void Hardware_Init(void)
+
+
+
+void Hardware_Init()
 {
 	// Set up output pins
 	RCC->IOPENR |= BIT0; // peripheral clock enable for port A
@@ -67,6 +70,45 @@ void Hardware_Init(void)
 	GPIOA->OTYPER &= ~BIT11; // Push-pull
     GPIOA->MODER = (GPIOA->MODER & ~(BIT24|BIT25)) | BIT24; // Make pin PA12 output (page 200 of RM0451, two bits used to configure: bit0=1, bit1=0)
 	GPIOA->OTYPER &= ~BIT12; // Push-pull
+
+	// Set up input pins
+	
+
+	//Button for switching modes
+	GPIOA->MODER &= ~(BIT26 | BIT27); // Make pin PA13 input button
+	// Activate pull up for pin PA14:
+	GPIOA->PUPDR |= BIT26;
+	GPIOA->PUPDR &= ~(BIT27);
+
+	//Button for left
+	GPIOA->MODER &= ~(BIT28 | BIT29); // Make pin PA14 input button
+	// Activate pull up for pin PA14:
+	GPIOA->PUPDR |= BIT28;
+	GPIOA->PUPDR &= ~(BIT29);
+
+	//Button for right
+	GPIOA->MODER &= ~(BIT30 | BIT31); // Make pin PA15 input button
+	// Activate pull up for pin PA15:
+	GPIOA->PUPDR |= BIT30;
+	GPIOA->PUPDR &= ~(BIT31);
+
+	// PB pins init
+	RCC->IOPENR |= RCC_IOPENR_GPIOBEN;
+	GPIOB->MODER &= ~GPIO_MODER_MODE3_0; // PB3 forward button
+	GPIOB->MODER &= ~GPIO_MODER_MODE4_0; // PB4 reverse button
+	GPIOB->PUPDR &= ~(GPIO_PUPDR_PUPD3_0 | GPIO_PUPDR_PUPD4_1);
+
+	/* //Button for forward
+	GPIOA->MODER &= ~(BIT28 | BIT29); // Make pin PA14 input button
+	// Activate pull up for pin PA14:
+	GPIOA->PUPDR |= BIT28;
+	GPIOA->PUPDR &= ~(BIT29);
+
+	//Button for reverse
+	GPIOA->MODER &= ~(BIT30 | BIT31); // Make pin PA15 input button
+	// Activate pull up for pin PA15:
+	GPIOA->PUPDR |= BIT30;
+	GPIOA->PUPDR &= ~(BIT31);*/
 
 	// Set up timer
 	RCC->APB1ENR |= BIT0;  // turn on clock for timer2 (UM: page 177)
@@ -80,10 +122,14 @@ void Hardware_Init(void)
 	__enable_irq();
 }
 
+
 int main(void)
 {
-    char buf[32];
+    char buff[32];
     int npwm;
+	int mode = 0;
+	int current, previous;
+
 
 	Hardware_Init();
 	
@@ -93,12 +139,38 @@ int main(void)
 
 	delayms(500); // Give putty a chance to start before we send characters with printf()
 	
-    printf("Servo signal generatioin using the STM32L051 using TIM2\r\n");
+    /*printf("Servo signal generatioin using the STM32L051 using TIM2\r\n");
     printf("(outputs are PA11 and PA12, pins 21 and 22).\r\n");
-    printf("By Jesus Calvino-Fraga (c) 2018-2023.\r\n\r\n");
-	
+    printf("By Jesus Calvino-Fraga (c) 2018-2023.\r\n\r\n");*/
+
+	previous=(GPIOB->IDR & GPIO_IDR_ID4)?0:1;
 	while (1)
 	{
-    	
+    	/*while button helde{
+
+			set left_flag
+			delay_ms(100)
+		}*/
+		current=(GPIOB->IDR & GPIO_IDR_ID4)?1:0;
+		while(current!=previous)
+		{
+			previous=current;
+			printf("PB4");
+			fflush(stdout);
+		}
+
+		/*while ((GPIOB->IDR & GPIO_IDR_ID4)?1:0) // mode change button
+			{
+
+				printf("%d\n\r", i);
+				i++;
+
+				// while (GPIOB->IDR & GPIO_IDR_ID4){
+
+				// }
+			}
+		i=0;
+		*/
+		delayms(100);
 	}
 }
